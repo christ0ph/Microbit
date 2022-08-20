3ac61ede837fcf1d7b7e776d7f2c2c2645355ac2
Christoph Schmitz <617462+christ0ph@users.noreply.github.com>
2022-08-20 14:53:37 +0200

    la til knapp A og logo

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+let kakeboks = 0
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    for (let index = 0; index < 5; index++) {
+        kakeboks = 1
+        basic.showNumber(kakeboks)
+        basic.pause(50)
+        kakeboks = 7
+        basic.showNumber(kakeboks)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     music.playTone(262, music.beat(BeatFraction.Half))
     music.playTone(294, music.beat(BeatFraction.Half))
